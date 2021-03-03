014c6826340719375f43cc93bf83e16bcc424b04
KingKeleos <kranzan99@gmail.com>
2021-03-03 15:54:54 +0100

    Projekt Initiiert

---
+++ Traveling Salesman/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+typedef struct // Forgegebene Struktur
+{
+    int from; // Index der Anfangsstadt
+    int to;   // Index der Zielstadt
+    int dist; // Entfernung der Städte
+    } Distance;
+
+typedef struct // Forgegebene Struktur
+{int n; // Anzahl der Städte
+char **cities; // Pointer auf Feld mit Städtenamen
+Distance *distances; //Pointer auf Feld mit Entfernungen
+} DistanceTable;
