968862910499814c108c4cf6b23335be2d86a440
KingKeleos <kranzan99@gmail.com>
2021-03-07 22:48:23 +0100

    Beginn Laden

--- Traveling Salesman/main.c
+++ Traveling Salesman/main.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <wchar.h>
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0; //test
+    printf("Travelingsalesman-Problem\n");
+    return 0;
 }
+
+void readTable()
+{
+int c;
+FILE *file;
+file=fopen("Staedte.txt","r");
+if(file){
+    while ((c=getc(file))!=EOF)
+        putchar(c);
+    fclose(file);
+}
+}
+
 
 typedef struct // Forgegebene Struktur
 {
