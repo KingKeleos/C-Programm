78314f743f2c97c13cf9bc6a4ddcc13dbec1b310
KingKeleos <kranzan99@gmail.com>
2021-03-08 14:58:59 +0100

    Erfolgreiches Laden derStrings

--- Traveling Salesman/main.c
+++ Traveling Salesman/main.c
@@ -7,34 +7,84 @@
 #include <wchar.h>
 
 
-int main()
+int main(void)
 {
-    printf("Travelingsalesman-Problem\n");
+    printf("\"Traveling Salesman\"-Problem\n");
+    char cities[90][90];
+    char c;
+    char string[15][256];
+    char pstring[15] = &string;
+    int i=0;
+    int j=0;
+    int k=0;
+    FILE *fptr;
+    fptr=fopen("../Staedte.txt","r");
+    if(fptr == NULL)
+    {
+        printf("Fehler, Datei nicht gefunden");
+        exit(1);
+    }
+    fseek(fptr, 0, SEEK_SET);
+    char table[15][15][256];
+    while ( (c=fgetc(fptr)) !=EOF) //Jeden Character Lesen, bis zum Ende der Datei
+    {
+        printf("Gelesener Charater: %c\n",c);
+        if (c==' ') //Wenn gelesener Character ein Leerzeichen -> vollen String in Tabelle eintragen.
+        {
+            i=i+1; //Eine Position weiter
+        }
+        if (c=='\n')
+        {
+            i=0; //wieder an den Anfang der Tabelle...
+            j=j+1; // ...aber eine Zeile weiter
+            printf("New Line\n");
+
+        }
+        else
+        {
+            table[i][j][k]=c;
+            k=k+1;
+        }
+    }
+
+    for(int i=0; i<sizeof(table);i=i+1)
+    {
+        for(int j=0; j<sizeof(table[i]);j+=1)
+            {
+                for (int k=0; k<sizeof(table[i][j]); k=k+1)
+                {
+                    printf("%s", table[i][j][k]);
+                }
+            }
+    }
+
+    int fclose(fptr);
     return 0;
 }
 
-void readTable()
+int readTable(char* filename)
 {
-int c;
-FILE *file;
-file=fopen("Staedte.txt","r");
-if(file){
-    while ((c=getc(file))!=EOF)
-        putchar(c);
-    fclose(file);
+    int c;
+    char cities[15][15];
+    FILE *file;
+    file=fopen(filename,"r");
+    int fgetc (file);
+    int fclose(file);
+    printf("Datei wurde gelesen");
+    return c;
 }
-}
 
 
-typedef struct // Forgegebene Struktur
-{
-    int from; // Index der Anfangsstadt
-    int to;   // Index der Zielstadt
-    int dist; // Entfernung der Städte
-    } Distance;
+//typedef struct // Forgegebene Struktur
+//{
+//    int from; // Index der Anfangsstadt
+//    int to;   // Index der Zielstadt
+//    int dist; // Entfernung der Städte
+//    } Distance;
 
-typedef struct // Forgegebene Struktur
-{int n; // Anzahl der Städte
-char **cities; // Pointer auf Feld mit Städtenamen
-Distance *distances; //Pointer auf Feld mit Entfernungen
-} DistanceTable;
+//typedef struct // Forgegebene Struktur
+//{
+//    int n; // Anzahl der Städte
+//    char **cities; // Pointer auf Feld mit Städtenamen
+//    Distance *distances; //Pointer auf Feld mit Entfernungen
+//    } DistanceTable;
