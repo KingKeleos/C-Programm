b2f0832977c3b4801f462d9172e69ee60623e4a3
KingKeleos <kranzan99@gmail.com>
2021-03-12 19:45:03 +0100

    Aufgabe a) Fertig

--- Traveling Salesman/main.c
+++ Traveling Salesman/main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 typedef struct // Forgegebene Struktur
 {
@@ -21,14 +22,75 @@ typedef struct // Forgegebene Struktur
     Distance *distances; //Pointer auf Feld vom Typ Distance
     } DistanceTable;
 
-bool checkReadability(FILE *fpointer)
+bool checkReadability(FILE *fpointer, DistanceTable* ptrTables)
 {
-    bool read;
-
-    return read;
+    int lines=0; //Zeilen Zähler
+    int i=0;
+    char clipboard[128]={0}; //Zwischenspeicher für die komplette Zeile
+    fseek(fpointer, 0, SEEK_SET); // Datei von Anfang an durchsuchen
+    while(fgets(clipboard, 128, fpointer)!=NULL)
+    {
+        lines++;
+    }
+    fseek(fpointer, 0, SEEK_SET); // Datei von Anfang an durchsuchen
+    fgets(clipboard, 128, fpointer);
+    char *string=strtok(clipboard, " \t");
+    while (string != NULL)
+    {
+        if(string[strlen(string)-1]=='\n')
+        {
+            string[strlen(string)-1]=0;
+        }
+        i++;
+        if (isdigit(*string)) //Wenn Integer in Namen -> Fehler
+        {
+            return false;
+        }
+        string=strtok(NULL, " \t");
+    }
+    if (lines != i+1) //Matrix in der falschen Dimension -> Fehler
+    {
+        return false;
+    }
+    int n=0; // Zählervariable für die Zeilen
+    int j=0; // Zählervariable für die Spalten
+    string=strtok(NULL, " \t");
+    while(fgets(clipboard, 128, fpointer)!=0)
+    {
+        string=strtok(clipboard, " \t");
+        while(string!=NULL)
+        {
+            if (n==j && strcmp(string,"0")) //Abstand Stadt zu sich selbst ist ungleich 0 -> Fehler
+            {
+                return false;
+            }
+            if (n!=j && strcmp(string,"0")==0) //Distanz von unterschiedlichen Staedten ist 0 -> Fehler
+            {
+                return false;
+            }
+            if (strcmp(string," ")==0)
+            {
+                return false;
+            }
+            int negcomp = atoi(string);
+            if (negcomp<0) //Negative Zahl finden -> Fehler
+            {
+                return false;
+            }
+            string=strtok(NULL, " \t");
+            j++;
+        }
+        if(j!=i) //Tabelle nicht Vollständig -> Fehler
+        {
+            return false;
+        }
+        j=0;
+        n++;
+    }
+    return true;
 }
 
-void readCities(DistanceTable* tables)
+DistanceTable* readCities(DistanceTable* tables)
 {
     char filename[128];
     int lines=0;
@@ -38,12 +100,18 @@ void readCities(DistanceTable* tables)
     printf("Wie heisst die Datei: ");
     scanf("%s", filename); //Nutzer kann seine Datei aussuchen
     FILE *fptr=fopen(filename,"r"); //Ausgewählte datei wird nur gelesen
-    bool readable = checkReadability(fptr);
+    bool readable = checkReadability(fptr, tables);
     if(fptr == NULL)
     {
         printf("Fehler, Datei nicht gefunden!\n"); //Fehlerwurf, bei nicht existenter Zeile
         fflush(fptr);
-        return NULL;
+        tables =NULL;
+        return tables;
+    }
+    if (readable==false)
+    {
+        printf("Die Datei ist nicht zulaessig!");//Fehlerausgabe
+        exit(1);
     }
     fseek(fptr, 0, SEEK_SET); // Datei von Anfang an durchsuchen
     int i=0;
@@ -97,28 +165,37 @@ void readCities(DistanceTable* tables)
         n++;
     }
     fclose(fptr);
-    free(tables);
+    return tables;
 }
 
 void writeCities(DistanceTable* tables)
 {
-
+    printf("%d\n", tables->n);
+    for (int i=0; i<(tables->n);i++)
+    {
+        printf("\n %s ",tables->cities[i]);
+        for(int j=0; j<(tables->n*tables->n);j++)
+        {
+            printf("%d ",tables->distances[j].dist);
+        }
+    }
 }
 
 int main()
 {
-    printf("\"Traveling Salesman\"-Problem\n\n");
+    printf("\"Traveling Salesman\"-Problem\n");
     DistanceTable tables;
+    DistanceTable *ptrTables=NULL;
     char ans;
     while (ans!='a'||ans!='b'||ans!='c'||ans!='d'||ans!='e'||ans!='f')
     {
-        printf("Waehlen Sie ihre Aktion: \n(a) Entfernungstabelle laden\n(b) Entfernungstabellespeichern\n(c) Entfernungstabelle anzeigen\n(d) Entfernung wischen zwei Städten ändern\n(e) Kuerzeste Route berechnen\n(f) Programm beenden\n");
-        printf("\n Ihre Auswahl: ");
+        printf("\nWaehlen Sie ihre Aktion: \n(a) Entfernungstabelle laden\n(b) Entfernungstabellespeichern\n(c) Entfernungstabelle anzeigen\n(d) Entfernung wischen zwei Städten ändern\n(e) Kuerzeste Route berechnen\n(f) Programm beenden\n");
+        printf("\nIhre Auswahl: ");
         scanf("%s", &ans);
         switch(ans)
         {
-            case 'a': readCities(&tables); break;
-            case 'b': writeCities(&tables); break;
+            case 'a': ptrTables = readCities(&tables); break; //Function zum laden der Datei öffnen
+            case 'b': writeCities(&tables); break; //Funktion zum schreiben der Daten öffnen
             case 'c': break;
             case 'd': break;
             case 'e': break;
@@ -126,13 +203,12 @@ int main()
             default: printf("Bitte nur mit a,b,c,d,e oder f antworten\n"); ans=NULL; break;
         }
     }
-    /*for (int i=0; i<(tables.n);i++)
+    for (int i=0; i<(tables.n);i++)
     {
-        printf("\n %d %s ", tables.n+1,tables.cities);
-        for(int j=0; j<5;j++)
+        printf("\n%d %s ",tables.n,tables.cities[i]);
+        for(int j=0; j<(tables.n*tables.n);j++)
         {
-            printf("%d ", tables.distances[j-4].dist);
-        }
-    }*/
+            printf("%d ",tables.distances[j].dist);
+        }}
     return 0;
 }
